d36cf464a8b3e5cd011fe9c47c678538078f218f
Aravindhan Elayakumar <aravindhansai@gmail.com>
2020-05-02 12:49:19 -0700

    part1 chkpoint ex1.1-1.2

---
+++ part1/courseinfo/src/content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Part from './part';
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
+    </div>
+  );
+};
+
+export default Content;
---
+++ part1/courseinfo/src/header.js
@@ -0,0 +1,4 @@
+import React from 'react';
+const Header = (props) => <h1>{props.course}</h1>;
+
+export default Header;
